c85e815e45ac38ebbb3c5713b88bdd8e9c67e06f
Athul Anoop <athulanoop2020@gmail.com>
2026-02-14 21:57:09 +0530

    chore(state): remove deprecated layout reducer
    - Layout state now managed by Zustand stores
    - Functionality migrated to layoutStore

--- apps/desktop/frontend/src/state/layoutReducer.ts
+++
@@ -1,8 +0,0 @@
-export function layoutReducer(state: any, action: any) {
-  switch (action.type) {
-    case "toggle":
-      return { ...state, [action.pane]: !state[action.pane] };
-    default:
-      return state;
-  }
-}
